667524b82b371ec101f95b3b7762058ff7748593
Alexander Orr <alexanderorr@Alexanders-MacBook-Pro.local>
2019-01-29 09:46:24 -0700

    Added invisible anchor to side-by-sides.

--- wp-content/themes/clwp-child/layouts/layout-side-by-side.php
+++ wp-content/themes/clwp-child/layouts/layout-side-by-side.php
@@ -6,11 +6,14 @@
         $title = get_sub_field('title');
         $text = get_sub_field('text');
         $link = get_sub_field('link');
+
+        $hash_title = str_replace(' ', '-', strtolower($title));
   ?>
   <div class="row side-by-side">
     <div class="column small-12 medium-6" style="background-image: url('<?= $image['url']; ?>');"></div>
     <div class="column small-12 medium-6">
       <div>
+        <div id="<?= $hash_title; ?>" class="invisible-anchor"></div>
         <?php if($title): ?>
           <h4><?= $title; ?></h4>
         <?php endif; ?>
